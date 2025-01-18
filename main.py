77257bea9bf5afe6f1308f1eadd4fc616d9d492d
Przemysław Pająk <bespider@gmail.com>
2025-01-18 22:15:34 +0100

    add timer

--- main.py
+++ main.py
@@ -12,6 +12,8 @@ def main():
     print(f"Screen height: {SCREEN_HEIGHT}")
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -19,6 +21,7 @@ def main():
         screen.fill(color="black")
 
         pygame.display.flip()
+        dt = clock.tick(60) / 1000
 
 
 if __name__ == "__main__":
